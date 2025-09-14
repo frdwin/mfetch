5a7fcf4a49237af4430406da194d22048f703495
Frederico Winter <frederico@diaswinter.com.br>
2025-09-14 16:03:33 -0300

    formatted the output to the maximum length of labels in config.h

--- src/mfetch.c
+++ src/mfetch.c
@@ -1,14 +1,18 @@
-//os, uptime, pkg
-// formatar o output
+//uptime, pkg
 // possibilitar uma ascii art customizada, com X linhas em um arquivo
 #include <stdio.h>
 #include <sys/utsname.h>
+#include <string.h>
 
 #include "config.h"
 #include "user.h"
 #include "shell.h"
 #include "kernel.h"
 #include "os.h"
+
+// Macro to calculate the size of the label and return the biggest label.
+#define LABEL_LENGTH(l) (int)(strlen(l))
+#define MAX_LABEL(l1, l2) ((LABEL_LENGTH(l1) > LABEL_LENGTH(l2)) ? l1 : l2)
 
 // /proc/uptime
 // xbps-query (tem alguma base em arquivo?)
@@ -17,36 +21,46 @@ int main(void)
 {
   int n;
 
+  // Calculate the maximum label length the user setted in config.h.
+  const char *max_label = MAX_LABEL(USER_LABEL, SHELL_LABEL);
+  max_label = MAX_LABEL(max_label, OS_LABEL);
+  max_label = MAX_LABEL(max_label, KERNEL_LABEL);
+  int max_len = LABEL_LENGTH(max_label);
+
   // Fetch and print username.
   int ulen = 30;
   char username[ulen];
+  printf("%*s ", max_len, USER_LABEL);
   if ((n = fetch_user(username, ulen)) == 0)
-    printf("%s %s\n", USER_LABEL, username);
+    printf("%s\n", username);
   else
-    printf("%s %s\n", USER_LABEL, "error");
+    printf("%s\n", "error");
 
   // Fetch and print shell name.
   int slen = 10;
   char shell_name[slen];
+  printf("%*s ", max_len, SHELL_LABEL);
   if ((n = fetch_shell(shell_name, slen)) == 0)
-    printf("%s %s\n", SHELL_LABEL, shell_name);
+    printf("%s\n", shell_name);
   else
-    printf("%s %s\n", SHELL_LABEL, "error");
+    printf("%s\n", "error");
 
   // Fetch and print OS name.
   int olen = 30;
   char os_name[olen];
+  printf("%*s ", max_len, OS_LABEL);
   if ((n = fetch_os(os_name, olen)) == 0)
-    printf("%s %s\n", OS_LABEL, os_name);
+    printf("%s\n", os_name);
   else
-    printf("%s %s\n", OS_LABEL, "error");
+    printf("%s\n", "error");
 
   // Fetch and print kernel release number.
   char kern_release[_UTSNAME_RELEASE_LENGTH];
+  printf("%*s ", max_len, KERNEL_LABEL);
   if ((n = fetch_kernel(kern_release)) == 0)
-    printf("%s %s\n", KERNEL_LABEL, kern_release);
+    printf("%s\n", kern_release);
   else
-    printf("%s %s\n", KERNEL_LABEL, "error");
+    printf("%s\n", "error");
 
   return 0;
 }
